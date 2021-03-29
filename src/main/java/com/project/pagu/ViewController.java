f7eb0c326e8b14bba80503ff5fe2636096c414ae
jihoho <xkftn94@naver.com>
2021-03-29 18:43:40 +0900

    feat: 메인 페이지 body 스켈레톤

---
+++ src/main/java/com/project/pagu/ViewController.java
@@ -0,0 +1,24 @@
+package com.project.pagu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: hojun
+ * Date: 2021-03-29 Time: 오후 3:38
+ */
+@Controller
+public class ViewController {
+
+    @GetMapping("/main-body")
+    public ModelAndView mainBody(HttpServletRequest request, HttpServletResponse response){
+        ModelAndView mav=new ModelAndView("main-body");
+        System.out.println("main-body");
+        return mav;
+    }
+}
