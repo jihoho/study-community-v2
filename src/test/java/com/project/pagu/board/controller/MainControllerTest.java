fc64dbf114a5872e82847d8571184bade6023fb9
yhh1056 <yhh1056@naver.com>
2021-04-07 14:36:23 +0900

    Refac: MemberControllerTest 수정

--- src/test/java/com/project/pagu/board/controller/MainControllerTest.java
+++ src/test/java/com/project/pagu/board/controller/MainControllerTest.java
@@ -41,30 +41,4 @@ class MainControllerTest {
                 .andDo(print());
     }
 
-    @DisplayName("회원 가입 페이지로 이동한다.")
-    @Test
-    void sign_up() throws Exception {
-        mockMvc.perform(get("/sign-up"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("sign-up"))
-                .andExpect(content().string(containsString("이메일")))
-                .andExpect(content().string(containsString("닉네임")))
-                .andExpect(content().string(containsString("비밀번호")))
-                .andDo(print());
-    }
-
-    @DisplayName("로그인 페이지로 이동한다.")
-    @Test
-    void login() throws Exception {
-        mockMvc.perform(get("/login"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("login"))
-                .andExpect(content().string(containsString("이메일")))
-                .andExpect(content().string(containsString("비밀번호")))
-                .andExpect(content().string(containsString("로그인")))
-                .andExpect(content().string(containsString("구글 로그인")))
-                .andExpect(content().string(containsString("비밀번호 찾기")))
-                .andExpect(content().string(containsString("회원가입")))
-                .andDo(print());
-    }
 }
--- src/test/java/com/project/pagu/member/controller/MemberControllerTest.java
+++ src/test/java/com/project/pagu/member/controller/MemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.project.pagu.member.controller;
 
+import static com.project.pagu.util.MultiValueMapConverter.convert;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -13,13 +14,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.pagu.member.repository.MemberRepository;
-import com.project.pagu.member.domain.MemberId;
-import com.project.pagu.member.domain.MemberType;
-import com.project.pagu.email.service.EmailService;
-import com.project.pagu.member.service.MemberService;
-import com.project.pagu.util.MultiValueMapConverter;
 import com.project.pagu.member.model.MemberSaveRequestDto;
+import com.project.pagu.member.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,7 +26,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.MultiValueMap;
 
@@ -43,24 +38,192 @@ import org.springframework.util.MultiValueMap;
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class)
-        //@WebMvcTest
 class MemberControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
     @Autowired
     private ObjectMapper objectMapper;
+
     @MockBean
     private MemberService memberService;
-    @MockBean
-    private EmailService emailService;
 
-    private MockHttpSession httpSession = new MockHttpSession();
-    private MemberSaveRequestDto dto;
+    private MemberSaveRequestDto memberSaveDto;
+
+    @BeforeEach
+    @DisplayName("memberSaveDto 정상 입력 세팅")
+    void beforeEach() {
+        memberSaveDto = new MemberSaveRequestDto();
+        memberSaveDto.setEmail("yy123@email.com");
+        memberSaveDto.setNickname("nick");
+        memberSaveDto.setPassword("abcde1234!");
+        memberSaveDto.setPasswordCheck("abcde1234!");
+        memberSaveDto.setAuthKey("123456");
+        memberSaveDto.setAuthKeyInput("123456");
+    }
+
+    @DisplayName("회원 가입 페이지로 이동한다.")
+    @Test
+    void sign_up() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("sign-up"))
+                .andExpect(content().string(containsString("이메일")))
+                .andExpect(content().string(containsString("닉네임")))
+                .andExpect(content().string(containsString("비밀번호")))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원가입 Form validation 성공 테스트")
+    void formValidationSuccessTest() throws Exception {
+        // given
+        MultiValueMap<String, String> params = convert(objectMapper, memberSaveDto);
+
+        mockMvc.perform(post("/members/valid")
+                .with(csrf())
+                .params(params))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(model().errorCount(0))
+                .andExpect(view().name("redirect:/email-check")); // email-check 페이지 이동
+    }
+
+    @Test
+    @DisplayName("가입하는 이메일이 중복일 경우 다시 가입창으로 이동한다.")
+    void uniqueEmailValidationFailTest() throws Exception {
+        // given
+        MultiValueMap<String, String> params = convert(objectMapper, memberSaveDto);
+
+        // when
+        when(memberService.existsByEmail(any())).thenReturn(true);
+
+        // then
+        mockMvc.perform(post("/members/valid")
+                .with(csrf())
+                .params(params))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasFieldErrorCode(
+                        "memberSaveRequestDto",
+                        "email",
+                        "UniqueEmail"))
+                .andExpect(content().string(containsString("이미 존재하는 이메일입니다.")))
+                .andExpect(view().name("sign-up"));
+    }
+
+    @Test
+    @DisplayName("가입하는 닉네임이 중복일 경우 다시 가입창으로 이동한다.")
+    void uniqueNicknameValidationFailTest() throws Exception {
+        // given
+        MultiValueMap<String, String> params = convert(objectMapper, memberSaveDto);
+
+        // when
+        when(memberService.existsByNickname(any())).thenReturn(true);
+
+        // then
+        mockMvc.perform(post("/members/valid")
+                .with(csrf())
+                .params(params))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasFieldErrorCode(
+                        "memberSaveRequestDto",
+                        "nickname",
+                        "UniqueNickname"))
+                .andExpect(content().string(containsString("이미 존재하는 닉네임입니다.")))
+                .andExpect(view().name("sign-up"));
+    }
+
+    @Test
+    @DisplayName("회원가입시 닉네임 패턴이 잘 못된 경우 다시 가입창으로 이동한다.")
+    void nicknamePatternFailTest() throws Exception {
+        // given
+        memberSaveDto.setNickname("nick&*");
+        MultiValueMap<String, String> params = convert(objectMapper, memberSaveDto);
+
+        mockMvc.perform(post("/members/valid")
+                .with(csrf())
+                .params(params))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasFieldErrorCode(
+                        "memberSaveRequestDto",
+                        "nickname",
+                        "Pattern"))
+                .andExpect(view().name("sign-up"));
+    }
+
+    @Test
+    @DisplayName("회원가입시 비밀번호 패턴이 잘 못된 경우 다시 가입창으로 이동한다.")
+    void passwordPatternFailTest() throws Exception {
+        // given
+        memberSaveDto.setPassword("123");
+        memberSaveDto.setPasswordCheck("123");
+        MultiValueMap<String, String> params = convert(objectMapper, memberSaveDto);
+
+        mockMvc.perform(post("/members/valid")
+                .with(csrf())
+                .params(params))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasFieldErrorCode(
+                        "memberSaveRequestDto",
+                        "password",
+                        "Pattern"))
+                .andExpect(view().name("sign-up"));
+    }
+
+    @Test
+    @DisplayName("회원가입시 비밀번호와 비밀번호 확인이 다른 경우 다시 가입창으로 이동한다.")
+    void passwordCheckFailTest() throws Exception {
+        // given
+        memberSaveDto.setPasswordCheck("differentPassword12!");
+        MultiValueMap<String, String> params = convert(objectMapper, memberSaveDto);
+
+        mockMvc.perform(post("/members/valid")
+                .with(csrf())
+                .params(params))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasFieldErrorCode(
+                        "memberSaveRequestDto",
+                        "passwordCheck",
+                        "NotEqualsPassword"))
+                .andExpect(content().string(containsString("비밀번호가 다릅니다.")))
+                .andExpect(view().name("sign-up"));
+    }
+
+    @Test
+    @DisplayName("이메일 인증 성공 테스트")
+    void emailCheckSuccessTest() throws Exception {
+        // when
+        MultiValueMap<String, String> params = convert(objectMapper, memberSaveDto);
+        // then
+        mockMvc.perform(post("/members/email-check")
+                .with(csrf())
+                .params(params))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(model().errorCount(0))
+                .andExpect(view().name("redirect:/sign-up-success")); // 이메일 인증 성공 후 profile 페이지로 이동
+    }
+
+    @Test
+    @DisplayName("이메일 인증이 실패하는 경우 페이지가 유지된다.")
+    void emailCheckFailTest() throws Exception {
+        // given
+        memberSaveDto.setAuthKeyInput("123123");
+        MultiValueMap<String, String> params = convert(objectMapper, memberSaveDto);
+
+        mockMvc.perform(post("/members/email-check")
+                .with(csrf())
+                .params(params))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasFieldErrorCode(
+                        "memberSaveRequestDto",
+                        "authKeyInput",
+                        "NotEqualsAuthKeyInput"))
+                .andExpect(view().name("email-check"));
+    }
 
     @DisplayName("프로필 페이지로 이동한다.")
     @Test
-    void login() throws Exception {
+    void profile() throws Exception {
         mockMvc.perform(get("/profile"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("profile"))
@@ -73,152 +236,19 @@ class MemberControllerTest {
                 .andDo(print());
     }
 
-    @BeforeEach
-    void beforeEach() {
-        // given: 테스트마다 dto set
-        dto = new MemberSaveRequestDto();
-        dto.setEmail("123@email.com");
-        dto.setNickname("nick");
-        dto.setPassword("abcde1234!");
-        dto.setPasswordCheck("abcde1234!");
-        dto.setAuthKey("123456");
-        dto.setAuthKeyInput("123456");
-    }
-
+    @DisplayName("로그인 페이지로 이동한다.")
     @Test
-    @DisplayName("가입하는 이메일이 중복일 경우 다시 가입창으로 이동한다.")
-    void uniqueEmailValidationFailTest() throws Exception {
-        // given
-        MultiValueMap<String, String> params = MultiValueMapConverter.convert(objectMapper, dto);
-
-        // when
-        when(memberService.existsByEmail(any())).thenReturn(true);
-
-        // then
-        mockMvc.perform(post("/members/valid")
-                .with(csrf())
-                .params(params))
+    void login() throws Exception {
+        mockMvc.perform(get("/login"))
                 .andExpect(status().isOk())
-                .andExpect(model().hasErrors())
-                .andExpect(content().string(containsString("이미 존재하는 이메일입니다.")))
-                .andExpect(view().name("sign-up"));
-    }
-
-    @Test
-    @DisplayName("가입하는 닉네임이 중복일 경우 다시 가입창으로 이동한다.")
-    void uniqueNicknameValidationFailTest() throws Exception {
-        // given
-        MultiValueMap<String, String> params = MultiValueMapConverter.convert(objectMapper, dto);
-
-        // when
-        when(memberService.existsByNickname(any())).thenReturn(true);
-
-        // then
-        mockMvc.perform(post("/members/valid")
-                .with(csrf())
-                .params(params))
-                .andExpect(status().isOk())
-                .andExpect(model().hasErrors())
-                .andExpect(content().string(containsString("이미 존재하는 닉네임입니다.")))
-                .andExpect(view().name("sign-up"));
-    }
-
-
-    @Test
-    @DisplayName("비밀번호 패턴 validation 실패 테스트")
-    void passwordPatternFailTest() throws Exception {
-        // given: 비밀번호 특수문자 미포함 invalid
-        dto.setPassword("123");
-        dto.setPasswordCheck("123");
-        MultiValueMap<String, String> params = MultiValueMapConverter.convert(objectMapper, dto);
-
-        mockMvc.perform(post("/members/valid").with(csrf())
-                .params(params))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeHasFieldErrorCode("memberSaveRequestDto", "password",
-                        "Pattern"))
-                .andExpect(view().name("sign-up")); // 다시 가입창으로 이동
-
-
-    }
-
-    @Test
-    @DisplayName("회원가입시 비밀번호와 비밀번호 확인이 다른 경우 다시 가입창으로 이동한다.")
-    void passwordCheckFailTest() throws Exception {
-        // given
-        dto.setPasswordCheck("differentPassword12!");
-        MultiValueMap<String, String> params = MultiValueMapConverter.convert(objectMapper, dto);
-
-        mockMvc.perform(post("/members/valid")
-                .with(csrf())
-                .params(params))
-                .andExpect(status().isOk())
-                .andExpect(model().hasErrors())
-                .andExpect(content().string(containsString("비밀번호가 다릅니다.")))
-                .andExpect(view().name("sign-up"));
-    }
-
-    @Test
-    @DisplayName("닉네임 pattern validation 실패 테스트")
-    void nicknamePatternFailTest() throws Exception {
-        // given: 닉네임 _-이외의 특수 문자 포함
-        dto.setNickname("nick&*");
-        MultiValueMap<String, String> params = MultiValueMapConverter.convert(objectMapper, dto);
-
-        mockMvc.perform(post("/members/valid").with(csrf())
-                .params(params))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeHasFieldErrorCode("memberSaveRequestDto", "nickname",
-                        "Pattern"))
-                .andExpect(view().name("sign-up")); // 다시 가입창으로 이동
-    }
-
-
-    @Test
-    @DisplayName("회원가입 Form validation 성공 테스트")
-    void formValidationSuccessTest() throws Exception {
-        // given: 모든 필드 valid
-        MultiValueMap<String, String> params = MultiValueMapConverter.convert(objectMapper, dto);
-        // when
-        when(memberService.save(any()))
-                .thenReturn(new MemberId(dto.getEmail(), MemberType.NORMAL));
-
-        mockMvc.perform(post("/members/valid").with(csrf())
-                .params(params))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(model().errorCount(0))
-                .andExpect(view().name("redirect:/email-check")); // email-check 페이지 이동
-    }
-
-
-    @Test
-    @DisplayName("이메일 인증이 실패하는 경우 페이지가 유지된다.")
-    void emailCheckFailTest() throws Exception {
-        // given
-        dto.setAuthKeyInput("123123");
-        MultiValueMap<String, String> params = MultiValueMapConverter
-                .convert(objectMapper, dto);
-
-        mockMvc.perform(post("/members/email-check")
-                .with(csrf())
-                .params(params))
-                .andExpect(status().isOk())
-                .andExpect(model().hasErrors())
-                .andExpect(view().name("email-check"));
-    }
-
-    @Test
-    @DisplayName("이메일 인증 성공 테스트")
-    void emailCheckSuccessTest() throws Exception {
-        // when
-        MultiValueMap<String, String> params = MultiValueMapConverter
-                .convert(objectMapper, dto);
-        // then
-        mockMvc.perform(post("/members/email-check").with(csrf())
-                .params(params))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(model().errorCount(0))
-                .andExpect(view().name("redirect:/sign-up-success")); // 이메일 인증 성공 후 profile 페이지로 이동
+                .andExpect(view().name("login"))
+                .andExpect(content().string(containsString("이메일")))
+                .andExpect(content().string(containsString("비밀번호")))
+                .andExpect(content().string(containsString("로그인")))
+                .andExpect(content().string(containsString("구글 로그인")))
+                .andExpect(content().string(containsString("비밀번호 찾기")))
+                .andExpect(content().string(containsString("회원가입")))
+                .andDo(print());
     }
 
 }
