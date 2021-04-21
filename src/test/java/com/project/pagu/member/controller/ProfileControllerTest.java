eebe4f6b8691c83f5e50c5c08ef528e3b071b707
yhh1056 <yhh1056@naver.com>
2021-04-21 13:49:54 +0900

    Test: 프로필 수정에 성공한다.

--- src/test/java/com/project/pagu/member/controller/ProfileControllerTest.java
+++ src/test/java/com/project/pagu/member/controller/ProfileControllerTest.java
@@ -1,14 +1,19 @@
 package com.project.pagu.member.controller;
 
+import static com.project.pagu.util.MultiValueMapConverter.convert;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.pagu.member.mockMember.WithMember;
+import com.project.pagu.member.model.ProfileRequestDto;
 import com.project.pagu.member.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.MultiValueMap;
 
 /**
  * Created by IntelliJ IDEA
@@ -32,6 +38,9 @@ class ProfileControllerTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @DisplayName("권한이 없는 사용자가 프로필 페이지로 이동 시 로그인 페이지로 redirect 된다.")
     @Test
@@ -60,6 +69,32 @@ class ProfileControllerTest {
                 .andExpect(view().name("profile"))
                 .andExpect(model().attributeExists("profileRequestDto"))
                 .andDo(print());
+    }
+
+    @WithMember
+    @Test
+    @DisplayName("프로필 수정에 성공한다.")
+    void profile_update() throws Exception {
+        MultiValueMap<String, String> params = convert(objectMapper, givenDto());
+
+        mockMvc.perform(post("/members/update")
+                .with(csrf())
+                .params(params))
+                .andExpect(status().is3xxRedirection());
+    }
+
+
+    private ProfileRequestDto givenDto() {
+        return ProfileRequestDto.builder()
+                .email("test@email.com")
+                .memberType("NORMAL")
+                .nickname("tester")
+                .changeNickname("nickname")
+                .career("취준생")
+                .position("백엔드")
+                .link("test@gi.com")
+                .info("안녕하세요")
+                .build();
     }
 
 }
